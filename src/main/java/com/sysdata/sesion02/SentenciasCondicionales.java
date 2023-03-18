c32cd2dc752c63ce16a2a508645e2d4cd32d2bb8
xedlz <3lxedlz@gmail.com>
2023-03-18 18:28:51 -0500

    Sentencias condicionales

--- src/main/java/com/sysdata/sesion02/SentenciasCondicionales.java
+++ src/main/java/com/sysdata/sesion02/SentenciasCondicionales.java
@@ -17,6 +17,24 @@ public class SentenciasCondicionales {
             System.out.println("son diferentes");
 
         }
+
+        // comparación de objetos
+        // cadenas
+
+        String cadena1 = "";
+        String cadena2 = "";
+        if(cadena1.equals(cadena2))
+        {
+            System.out.println("las cadenas son iguales");
+        }
+        // comparamos las referencias a los objetos
+        if(cadena1 == cadena2)
+        {
+            System.out.println("las cadenas son iguales");
+        }
+        else{
+            System.out.println("las cadenas son diferentes");
+        }
     }
 
 }
