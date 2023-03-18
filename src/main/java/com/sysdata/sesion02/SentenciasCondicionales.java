bd41adeb109098560466acbf36ed7b0acc7b4842
xedlz <3lxedlz@gmail.com>
2023-03-18 18:22:15 -0500

    Sentencias condicionales

---
+++ src/main/java/com/sysdata/sesion02/SentenciasCondicionales.java
@@ -0,0 +1,22 @@
+package com.sysdata.sesion02;
+
+public class SentenciasCondicionales {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+        // comparación igualdad
+        if(i == j){
+            System.out.println("son iguales");
+
+        }
+        //comparación desigualdad
+         i = 0;
+         j = 1;
+        if(i != j){
+            System.out.println("son diferentes");
+
+        }
+    }
+
+}
