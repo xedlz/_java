0c8dc9c7f71f66763bc4d15b9d36ca4f1045f662
xedlz <3lxedlz@gmail.com>
2023-03-18 09:58:31 -0500

    tipo de variables

---
+++ src/main/java/com/sysdata/Variables.java
@@ -0,0 +1,37 @@
+package com.sysdata;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("ejemplos de variables");
+        // declaracion de la variable
+        int numero;
+        // inicializacion de la variable
+        numero = 12;
+        // imprimir una variable por pantalla
+        System.out.println(numero);
+
+        // tipos de variabes
+        System.out.println("Ejemplos de variables booleanas");
+
+        boolean activo =  true;
+        // cambiar el valor de la variable
+        activo = false;
+
+        System.out.println(activo);
+
+        // variables numericas
+        // numeros enteros
+
+        float flotante = 12.8F;
+
+        System.out.println(flotante);
+
+        double doble = 3.144522;
+        System.out.println(doble);
+
+        // caracteres (unicode)
+
+
+
+    }
+}
